c4b5a8e2b1fec4cbc61423365c8a58793993beff
Marco Taddei <m.taddei92@gmail.com>
2014-05-22 12:14:40 +0200

    LALALALALALALALALALALALALA

--- Progetto/bdlab-lib-fun.php
+++ Progetto/bdlab-lib-fun.php
@@ -1,22 +1,12 @@
 <?php
 include_once('conf.php');
-
-$users=array(
-	'Marco' => 'lollipop',
-    'steve' => '123',
-    'john' => 'abc');
-
-$category=array(
-	'Marco' => 'admin',
-    'steve' => 'admin',
-    'john' => 'user');
   
 function user_check($name, $pass) {
 //verifica la correttezza di login e password inserite e imposta il contenuto della sessione
 
 	$db = connection_pgsql();
 	
-	$sql = "SELECT pwd, nome FROM progetto_db.utente WHERE mail = $1";
+	$sql = "SELECT pwd FROM progetto_db.utente WHERE mail = $1";
 	$result = pg_prepare($db, "q", $sql);
 	$value = array($name);
 	$result = pg_execute($db, "q", $value);
@@ -28,19 +18,24 @@ function user_check($name, $pass) {
 	print_r ($row);
 	
 	if($row['pwd'] == $pass){
-		$_SESSION['isLogged'] = $row['nome'];
+		$_SESSION['isLogged'] = $name;
 		return true;
 	}
 	else
 		return false;
 }
   
-function user_category($name)  {
+/* function user_category($name)  {
 //restituisce la categoria dell'utente specificato in input
     
 global $category;
 
     return $category[$name]; 
+
+}*/
+
+function print_user_data($username){
+
 
 }
 
