1b06ba261cc883079ef660155cac9adf1cd89265
Marco Taddei <m.taddei92@gmail.com>
2014-06-06 10:59:47 +0200

    Trigger

--- Progetto - SQL/progetto_trigger.sql
+++ Progetto - SQL/progetto_trigger.sql
@@ -1,23 +1,17 @@
------------------------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION insert_conto_deposito() RETURNS TRIGGER AS $$
+-------------
+
+CREATE OR REPLACE FUNCTION insert_root_categories() RETURNS TRIGGER AS $$
 
 BEGIN
-	INSERT INTO conto VALUES (NEW.iban, 'Deposito', NEW.mail);
+	INSERT INTO categoria VALUES ('Spesa', '-', NEW.mail, NULL, NULL);
+	INSERT INTO categoria VALUES ('Entrata', '+', NEW.mail, NULL, NULL);
 	RETURN NEW;
 END;
 
-$$LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
 
-CREATE TRIGGER assegna_conto_deposito BEFORE INSERT ON conto_deposito FOR EACH ROW EXECUTE PROCEDURE insert_conto_deposito();
+CREATE TRIGGER create_root_categories AFTER INSERT ON utente FOR EACH ROW EXECUTE PROCEDURE insert_root_categories();
 
 -------------
-CREATE OR REPLACE FUNCTION insert_conto_credito() RETURNS TRIGGER AS $$
 
-BEGIN
-	INSERT INTO conto VALUES (NEW.iban, 'Credito', NEW.mail);
-	RETURN NEW;
-END;
-
-$$LANGUAGE 'plpgsql';
-
-CREATE TRIGGER assegna_conto_credito BEFORE INSERT ON conto_credito FOR EACH ROW EXECUTE PROCEDURE insert_conto_credito();
+CREATE TRIGGER rinnova_bilancio WHEN 
